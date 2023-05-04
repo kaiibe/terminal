787421d053bd615e02d57b9e8a0fa3677ed64a1e
kaiibe <kaiibe@mail.ru>
2023-05-04 15:30:18 +0200

    first commit

---
+++ js/commands.js
@@ -0,0 +1,85 @@
+const instagramUrl = "https://www.instagram.com/_kaiibe__/";
+const linkedinUrl = "https://www.linkedin.com/in/behzod-khomidov-bk/";
+const githubUrl = "https://github.com/kaiibe/";
+const codeforcesUrl = "https://codeforces.com/profile/Beha_888";
+const leetcodeUrl = "https://leetcode.com/kaiibe";
+
+email = "mailto:kaiibe@mail.ru";
+error =
+  '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'ls\'</span>.</span>';
+
+(ls = [
+  "<br>",
+  "<span class='command'>ls</span>              Display list of commands",
+  "<span class='command'>whoami</span>          Who is Behzod?",
+  "<span class='command'>social</span>          My social networks",
+  "<span class='command'>projects</span>        My coding projects",
+  "<span class='command'>email</span>           Do not email me",
+  "<span class='command'>history</span>         View command history",
+  "<span class='command'>banner</span>          Display the header",
+  "<span class='command'>clear</span>           Clear terminal",
+  "<br>",
+]),
+  (whoami = [
+    "<br>",
+    "Hey, I'm Behzod👋 <br><br>",
+    "I am a student currently pursuing a degree in Software Engineering at the University <br>",
+    "of Europe for Applied Sciences. I am a hardworking individual who is passionate about <br>",
+    "developing my skills in the field of Software Engineering. <br><br>",
+    "As a part of my journey, I have been solving programming questions on platforms like <br>",
+    "<a href='" +
+      codeforcesUrl +
+      "' target='_blank'>CodeForces.com</a> and <a href='" +
+      leetcodeUrl +
+      "' target='_blank'>LeetCode.com</a>. This has helped me to expand my knowledge and develop <br>",
+    "my problem-solving skills. In addition to my studies, I also engage in <a href='" +
+      githubUrl +
+      "' target='_blank'>personal projects</a> <br>",
+    "to enhance my practical understanding of software development. I am eager to explore the <br>",
+    "latest technologies and trends in the field, and I look forward to applying my skills and <br>",
+    "knowledge to real-world projects. <br><br>",
+    "Outside of my studies and programming, I have several hobbies that I enjoy. I am an avid <br>",
+    "gym-goer and love to exercise and stay fit. Playing guitar is also a passion of mine, and I <br>",
+    "enjoy learning and practicing new songs in my free time. Additionally, I enjoy playing <br>",
+    "volleyball with friends and participating in local leagues. Overall, I strive to maintain a <br>",
+    "healthy work-life balance and pursue my passions both inside and outside of my field of study. <br>",
+    "<br>",
+  ]),
+  (social = [
+    "<br>",
+    "instagram       <a href='" +
+      instagramUrl +
+      "' target='_blank'>instagram/kaiibe</a>",
+    "linkedin        <a href=' " +
+      linkedinUrl +
+      " ' target='_blank'>linkedin/kaiibe</a>",
+    "github          <a href=' " +
+      githubUrl +
+      " ' target='_blank'>github/kaiibe</a>",
+    "codeforces      <a href=' " +
+      codeforcesUrl +
+      " ' target='_blank'>codeforces/kaiibe</a>",
+    "leetcode        <a href=' " +
+      leetcodeUrl +
+      " ' target='_blank'>leetcode/kaiibe</a>",
+    "<br>",
+  ]);
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on <a href='" +
+    githubUrl +
+    "' target='_blank'>GitHub</a>, or confidential.",
+  "<br>",
+];
+
+banner = [
+  "  _        _ _ _                _                      _             _ ",
+  "| | ____ _(_|_) |__   ___      | |_ ___ _ __ _ __ ___ (_)_ __   __ _| |",
+  "| |/ / _` | | | '_ \\ / _ \\     | __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` | |",
+  "|   < (_| | | | |_) |  __/     | ||  __/ |  | | | | | | | | | | (_| | |",
+  "|_|\\_\\__,_|_|_|_.__/ \\___|      \\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|_|",
+  "<br>",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'ls\'</span><span class="color2">.</span>',
+];
---
+++ js/main.js
@@ -0,0 +1,119 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer");
+var textarea = document.getElementById("texter");
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var commands = [];
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+    textarea.focus();
+  } else if (e.keyCode == 13) {
+    commands.push(command.innerHTML);
+    git = commands.length;
+    addLine("visitor@kaiibe.com:~$ " + command.innerHTML, "no-animation", 0);
+    commander(command.innerHTML.toLowerCase());
+    command.innerHTML = "";
+    textarea.value = "";
+  } else if (e.keyCode == 38 && git != 0) {
+    git -= 1;
+    textarea.value = commands[git];
+    command.innerHTML = textarea.value;
+  } else if (e.keyCode == 40 && git != commands.length) {
+    git += 1;
+    if (commands[git] === undefined) {
+      textarea.value = "";
+    } else {
+      textarea.value = commands[git];
+    }
+    command.innerHTML = textarea.value;
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "ls":
+      loopLines(ls, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine(
+        'Opening mailto:<a href="kaiibe@mail.ru">kaiibe@main.com</a>...',
+        "color2",
+        80
+      );
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(function () {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "",  80)
+      break;
+    default:
+      addLine(error, "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+}
